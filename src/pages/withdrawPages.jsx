11740602a6fb8e6d02dbe611ae84541a94b8d0b2
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-10 03:35:00 +0700

    Create withdrawPages.jsx

---
+++ src/pages/withdrawPages.jsx
@@ -0,0 +1,11 @@
+// src/pages/WithdrawPage.jsx
+import WithdrawRequest from "../components/WithdrawRequest";
+
+export default function WithdrawPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-6">Penarikan Dana Mitra</h1>
+      <WithdrawRequest />
+    </div>
+  );
+}
