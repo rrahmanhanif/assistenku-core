e7e974c85dd03c7cf93deaef2e42ec42b235b5b4
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-09 20:27:55 +0700

    Update DashboardAdmin.jsx

--- src/pages/DashboardAdmin.jsx
+++ src/pages/DashboardAdmin.jsx
@@ -18,4 +18,29 @@ const DashboardAdmin = () => {
   );
 };
 
+import MapMonitor from "../components/MapMonitor";
+import MitraList from "../components/MitraList";
+import CustomerList from "../components/CustomerList";
+import OrdersMonitor from "../components/OrdersMonitor";
+import Transactions from "./Transactions";
+
+const DashboardAdmin = () => {
+  return (
+    <div className="dashboard">
+      <h2>Assistenku-Core Dashboard</h2>
+      <OrdersMonitor />
+      <hr />
+      <Transactions />
+      <hr />
+      <MapMonitor />
+      <hr />
+      <MitraList />
+      <hr />
+      <CustomerList />
+    </div>
+  );
+};
+
+export default DashboardAdmin;
+
 export default DashboardAdmin;
