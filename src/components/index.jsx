80e7bcdeeab6d618be46712ae5d5eb154faa5896
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-11 21:03:33 +0700

    Create index.jsx

---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+// src/components/index.jsx
+// === KOMPONEN INTI ASSISTENKU-CORE ===
+// Semua komponen utama dikumpulkan di sini agar rapi dan mudah diimport di App.jsx
+
+export { default as AnalyticsChart } from "./analyticschart";
+export { default as CustomerList } from "./customerlist";
+export { default as MapMonitor } from "./mapmonitor";
+export { default as MitraList } from "./mitralist";
+export { default as Navbar } from "./navbar";
+export { default as Notification } from "./notification";
+export { default as PopupNotify } from "./popupnotify";
+export { default as TransactionTable } from "./transactiontable";
+export { default as WithdrawRequest } from "./withdrawrequest";
