6716eeb8af0fb73314face593c71b2241eca5043
rrahmanhanif <rrahmanhanif@gmail.com>
2025-11-22 20:51:53 +0700

    Update AdminLayout.jsx

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,16 +1,17 @@
-// src/layout/AdminLayout.jsx
-import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
-export default function AdminLayout({ children, onLogout, role }) {
+export default function AdminLayout({ children, onLogout }) {
   return (
     <div style={{ display: "flex" }}>
-      <Sidebar role={role} />
+      {/* Sidebar */}
+      <Sidebar />
 
+      {/* Main Content */}
       <div style={{ marginLeft: "240px", width: "100%" }}>
         <Navbar onLogout={onLogout} />
 
-        <div style={{ padding: "1.5rem" }}>
+        <div style={{ padding: "20px" }}>
           {children}
         </div>
       </div>
